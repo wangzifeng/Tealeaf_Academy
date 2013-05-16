86860bdf6141b35d8adc9244459ea0966433a151
Frank W <info@wangzifeng.com>
2013-05-16 16:44:35 +0800

    OO black jack revision

--- blackjack_oo.rb
+++ blackjack_oo.rb
@@ -3,7 +3,8 @@
 
 require "pry"
 
-module Game
+module FairPlay
+
 	def cal(cards)
 		arr = cards.map { |v| v[1]}
 		total = 0
@@ -16,11 +17,13 @@ module Game
 				total += v.to_i
 			end
 		end
+
 		arr.select { |x| x == "A"}.count.times do
 			total -= 10 if total > 21
 		end
 		total
 	end
+
 
 
 	def result(total, player)
@@ -37,12 +40,12 @@ module Game
 end
 
 
-class CasinoBoys;
-	def has_cards
-		@has_cards = []
-	end
+class CasinoBoys
+	attr_accessor :cards, :total, :name
 
-	def has_total
+	def initialize(name)
+		@name = name
+		@cards = []
 		@total = 0
 	end
 end
@@ -50,8 +53,8 @@ end
 
 #Dealer
 class Dealer < CasinoBoys
-	def initialize(name,level)
-		@name = name
+	def initialize(dealer_name, level)
+		super(dealer_name)
 		@level = level
 	end
 end
@@ -59,22 +62,20 @@ end
 #Player
 
 class Player < CasinoBoys
-	attr_accessor :ante, :fund
+	attr_accessor :fund, :ante
 
-	def initialize(name, fund, ante)
-		@name = name
+	def initialize(player_name, fund, ante)
+		super(player_name)
 		@fund = fund
 		@ante = ante
 	end
-
-
 
 end
 
 #The Cards
 
 class Cards
-	include Game
+	include FairPlay
 	attr_accessor :deck
 
 	def initialize
@@ -83,31 +84,51 @@ class Cards
 		@deck = suits.product(cards)
 	end
 
-	def hit
-		binding.pry
-		player_card = []
-		player_card << @deck.pop
-		player_card
+	def shuffle
+		@deck.shuffle!
 	end
 
-	def stay
-		puts "Stay"
+	def pop
+		@deck.pop
+	end
+
+end
+
+
+class Game
+	include FairPlay
+
+	def intro(player)
+		puts "Welcome! #{player.name}"
+	end
+
+	def game_on(player,deck)
+		intro(player)
+		player.cards << deck.pop
+		player.cards << deck.pop
+		while player.total < 21
+			binding.pry
+			puts "You want 1) hit or 2)stay?"
+			operation = gets.chomp.to_i
+			case operation
+			when "1"
+				player.total = cal(player.cards)
+				puts "Your total is #{player.total}"
+			when "2"
+				puts "Stay"
+				break
+			end
+		end
 	end
 end
 
 #The Game
 
+dealer = Dealer.new("Jack","Junior")
+player1 = Player.new("Frank",1000,10)
+game = Game.new
 
+deck = Cards.new.shuffle
+game.game_on(player1,deck)
 
-dealer1 = Dealer.new("Jack","Junior")
-
-player1 = Player.new("Frank",1000, 10)
-
-deck = Cards.new
-
-
-
-player1.has_cards << deck.hit
-
-p player1.has_cards
 
